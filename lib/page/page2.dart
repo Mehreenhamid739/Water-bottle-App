da77e4c66178bf264adbda0bbabb91373ecefc0d
Mehreen hamid <mehreenhusssain739@gmail.com>
2024-12-03 10:54:37 +0500

    Change code

--- lib/page/page2.dart
+++ lib/page/page2.dart
@@ -13,35 +13,30 @@ class Page2 extends StatelessWidget {
           borderRadius: BorderRadius.circular(10),
           child: Container(
             color: const Color.fromARGB(255, 0, 73, 2),
-            child: const Column(
+            child: Column(
               children: [
-                SizedBox(
+                const SizedBox(
                   height: 20,
                 ),
-                Text(
+                const Text(
                   'Mehreen',
                   style: TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                       fontSize: 20),
                 ),
-                SizedBox(
-                  height: 15,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(left: 10.0, right: 10),
-                  child: Text(
-                    "Mehreen is a talented Flutter developer with a passion for building cross-platform apps. Currently working with a company, she’s gaining real-world experience while steadily honing her skills. Driven to grow, she’s seeking an internship at a top software house to further expand her expertise. With her strong work ethic and eagerness to learn, Mehreen is ready to make an impact in the tech world",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.normal,
-                        fontSize: 16,
-                        letterSpacing: 1,
-                        wordSpacing: 2),
+                Center(
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://thumbs.dreamstime.com/b/hand-holding-glass-globe-ball-tree-growing-green-nature-blur-background-eco-concept-161081206.jpg"))),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.only(left: 10.0, right: 10),
+                const Padding(
+                  padding: EdgeInsets.only(left: 10.0, right: 10, top: 30),
                   child: Text(
                     "Mehreen is highly motivated to advance her career and is actively seeking an internship with a leading software house. ",
                     style: TextStyle(
